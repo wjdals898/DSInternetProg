16428a2161bd2ff89f8df7d4b875d8bd9abcaf3b
wjdals898 <jungmin898@naver.com>
2021-11-03 20:52:35 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/tests.py
+++ blog/tests.py
@@ -95,6 +95,29 @@ class TestView(TestCase):
         self.assertNotIn(self.post_002.title, main_area.text)
         self.assertNotIn(self.post_003.title, main_area.text)
 
+    def test_create_post(self):
+        # 포스트 목록 페이지를 가져온다
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
+        self.client.login(username='Trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        # 정상적으로 페이지가 로드
+        self.assertEqual(response.status_code, 200)
+        # 페이지 타이틀 'Blog'
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Create Post - Blog')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Create New Post', main_area.text)
+
+        self.client.post('/blog/create_post/',
+                         {
+                             'title' : 'Post form 만들기',
+                             'content' : "Post form 페이지 만들기"
+                         })
+        last_post = Post.objects.last()
+        self.assertEqual(last_post.title, "Post form 만들기")
+        self.assertEqual(last_post.author.username, 'Trump')
+
     def test_post_list(self):
         # 포스트가 3개 존재하는가
         self.assertEqual(Post.objects.count(), 3)
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [ # 서버IP/blog/
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),  # 서버 IP/blog
 
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
