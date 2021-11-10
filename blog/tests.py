7fccf8d0fb2e173019259de377ed45aec3737ad2
wjdals898 <jungmin898@naver.com>
2021-11-10 14:31:05 +0900

    포스트 수정 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -127,6 +127,42 @@ class TestView(TestCase):
         self.assertEqual(last_post.title, "Post form 만들기")
         self.assertEqual(last_post.author.username, 'James')
 
+    def test_update_post(self):
+        update_url = f'/blog/update_post/{self.post_003.pk}/'
+        # 로그인하지 않은 경우
+        response = self.client.get(update_url)
+        self.assertNotEqual(response.status_code, 200)
+
+        # 로그인 했지만 작성자가 아닌 경우
+        self.assertNotEqual(self.post_003.author, self.user_james)
+        self.client.login(username='James', password='somepassword')
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 403) # 403 : forbidden (접근 권한 금지)
+
+        # 로그인했고 작성자인 경우
+        self.client.login(username='Trump', password='somepassword')
+        response = self.client.get(update_url)
+        self.assertEqual(response.status_code, 200)
+
+        # 수정 페이지
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text, 'Edit Post - Blog')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('Edit Post', main_area.text)
+
+        # 실제 수정 후 확인
+        response = self.client.post(update_url,
+                         {
+                             'title': '세번째 포스트 수정',
+                             'content': '안녕? 우리는 하나/... 반가와요',
+                             'category': self.category_culture.pk
+                         }, follow=True)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('세번째 포스트 수정', main_area.text)
+        self.assertIn('안녕? 우리는 하나/... 반가와요', main_area.text)
+        self.assertIn(self.category_culture.name, main_area.text)
+
     def test_post_list(self):
         # 포스트가 3개 존재하는가
         self.assertEqual(Post.objects.count(), 3)
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [ # 서버IP/blog/
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),  # 서버 IP/blog
 
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page),
