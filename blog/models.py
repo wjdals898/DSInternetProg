2a3c2a260fdf55bdc8ebb871a986ba2eb77625f7
wjdals898 <jungmin898@naver.com>
2021-11-30 14:29:03 +0900

    대문 페이지 수정하기

--- blog/models.py
+++ blog/models.py
@@ -58,6 +58,12 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return 'https://doitdjango.com/avatar/id/414/d933050d58efc136/svg/{self.author.email}/'
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
